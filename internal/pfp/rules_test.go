5f9ceb3a90c5e68ddaf3a22519d5b73d4f17d0db
rhysj6 <rhysj6@gmail.com>
2026-02-19 01:21:10 +0000

    Refactor tests for Rule and LineMatcher to follow table pattern

--- internal/pfp/rules_test.go
+++ internal/pfp/rules_test.go
@@ -5,83 +5,99 @@ import (
 	"testing"
 )
 
-func HelperGetNeededLineCount(r Rule, e int, t *testing.T) {
-	m := r.GetNeededLineCount()
-
-	if r.GetNeededLineCount() != e {
-		t.Fatalf("Expected %v lines got %v", e, m)
-	}
-}
-
-func TestRuleGetNeededLineCountHandlesSingleCheck(t *testing.T) {
-	r := Rule{Checks: []LineMatcher{{Contains: "Hello?"}}, MaxLines: 100}
-
-	HelperGetNeededLineCount(r, 1, t)
-}
-
-func TestRuleGetNeededLineCountHandlesMoreHandlersThanLimit(t *testing.T) {
-	r := Rule{Checks: []LineMatcher{{}}, MaxLines: 0}
-
-	HelperGetNeededLineCount(r, 1, t)
-}
-
-func TestRuleGetNeededLineCountHandlesMaxLines(t *testing.T) {
-	r := Rule{
-		Checks:   []LineMatcher{{Contains: "Something"}, {Contains: "Something"}},
-		MaxLines: 17,
+func TestRuleGetNeededLineCount(t *testing.T) {
+	tests := []struct {
+		name     string
+		rule     Rule
+		expected int
+	}{
+		{
+			name:     "Handles single check",
+			rule:     Rule{Checks: []LineMatcher{{Contains: "Hello?"}}, MaxLines: 100},
+			expected: 1,
+		},
+		{
+			name:     "Handles more checks than limit",
+			rule:     Rule{Checks: []LineMatcher{{}}, MaxLines: 0},
+			expected: 1,
+		},
+		{
+			name:     "Handles max lines",
+			rule:     Rule{Checks: []LineMatcher{{Contains: "Something"}, {Contains: "Something"}}, MaxLines: 17},
+			expected: 17,
+		},
 	}
 
-	HelperGetNeededLineCount(r, 17, t)
-}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := tt.rule.GetNeededLineCount()
 
-func HelperCheckLine(lm LineMatcher, l string, e bool, t *testing.T) {
-	r := lm.CheckLine(l)
-
-	if r != e {
-		t.Fatalf("Expected %t but got %t", e, r)
+			if r != tt.expected {
+				t.Fatalf("Expected %v lines got %v", tt.expected, r)
+			}
+		})
 	}
+
 }
 
-func TestCheckLineHandlesContainsWithNoRegex(t *testing.T) {
-	line := "ERROR: Something failed"
-
-	HelperCheckLine(LineMatcher{Contains: "ERROR"}, line, true, t)
-
-	HelperCheckLine(LineMatcher{Contains: "ERROR: Something"}, line, true, t)
-
-	HelperCheckLine(LineMatcher{Contains: "INFO"}, line, false, t)
-}
-
-func TestCheckLineHandlesContainsWithRegex(t *testing.T) {
-	line := "ERROR: Something failed"
-
+func TestCheckLine(t *testing.T) {
 	re1, err := regexp.Compile(`ERROR:`)
 	re2, err := regexp.Compile(`ERROR:Hi`)
 	if err != nil {
 		t.Fatal("Error compiling regex")
 	}
 
-	HelperCheckLine(LineMatcher{Contains: "ERROR", Regex: re1}, line, true, t)
-
-	HelperCheckLine(LineMatcher{Contains: "ERROR:", Regex: re2}, line, false, t)
-}
-
-func TestCheckLineHandlesRegexWithNoContains(t *testing.T) {
-	line := "ERROR: Something failed"
-
-	re1, err := regexp.Compile(`ERROR:`)
-	re2, err := regexp.Compile(`ERROR:Hi`)
-	if err != nil {
-		t.Fatal("Error compiling regex")
+	tests := []struct {
+		name     string
+		lm       LineMatcher
+		expected bool
+	}{
+		{
+			name:     "Handle match contains with no regex",
+			lm:       LineMatcher{Contains: "ERROR"},
+			expected: true,
+		},
+		{
+			name:     "Handle no match contains with no regex",
+			lm:       LineMatcher{Contains: "INFO"},
+			expected: false,
+		},
+		{
+			name:     "Handle match contains with regex",
+			lm:       LineMatcher{Contains: "ERROR", Regex: re1},
+			expected: true,
+		},
+		{
+			name:     "Handle no match contains with regex",
+			lm:       LineMatcher{Contains: "ERROR", Regex: re2},
+			expected: false,
+		},
+		{
+			name:     "Handle match regex only",
+			lm:       LineMatcher{Regex: re1},
+			expected: true,
+		},
+		{
+			name:     "Handle no match regex only",
+			lm:       LineMatcher{Regex: re2},
+			expected: false,
+		},
+		{
+			name:     "Handle no values ",
+			lm:       LineMatcher{},
+			expected: false,
+		},
 	}
 
-	HelperCheckLine(LineMatcher{Regex: re1}, line, true, t)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			line := "ERROR: Something failed"
 
-	HelperCheckLine(LineMatcher{Regex: re2}, line, false, t)
-}
+			r := tt.lm.CheckLine(line)
 
-func TestCheckLineHandlesNoValues(t *testing.T) {
-	line := "ERROR: Something failed"
-
-	HelperCheckLine(LineMatcher{}, line, false, t)
+			if r != tt.expected {
+				t.Fatalf("Expected %t but got %t", tt.expected, r)
+			}
+		})
+	}
 }
