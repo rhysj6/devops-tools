b739eaa44c6f19caedcee3d36eb6df72bd8ffd6b
rhysj6 <rhysj6@gmail.com>
2026-02-23 22:44:27 +0000

    Made new RecursiveLogSource for log sources that can read downstream logs

--- internal/pfp/main.go
+++ internal/pfp/main.go
@@ -19,13 +19,16 @@ type Stats struct {
 type LogSource interface {
 	// Get the logs to be parsed. This should return an io.ReadCloser that can be used to read the logs line by line.
 	GetLogs() (io.ReadCloser, error)
-	// SupportDownstreamFailedBuilds returns true if the log source supports parsing logs of downstream failed builds.
-	// If this returns true, the parser will use the rule returned by GetDownstreamFailedBuildRule to identify log lines that indicate a downstream failed build and then use GetDownstreamFailedBuildLogs to get the logs of the downstream failed build.
-	SupportDownstreamFailedBuilds() bool
+}
+
+type RecursiveLogSource interface {
+	LogSource
 	// GetDownstreamFailedBuildRule returns the rule that should match a log line to be considered the results of a failed downstream build.
 	GetDownstreamFailedBuildRule() *Rule
 	// GetDownstreamFailedBuildLogs returns the logs of a downstream failed build given a ParseMatch that contains the rule that matched the failed downstream build and the log lines that matched that rule.
-	GetDownstreamFailedBuildLogs(*ParseMatch) (io.Reader, error)
+	GetDownstreamFailedBuildLogs(*ParseMatch) (io.ReadCloser, error)
+	// GetMaxRecursionDepth returns the maximum recursion depth for parsing downstream failed builds. This is to prevent infinite recursion in case of circular dependencies between builds. If not implemented, it defaults to 3, meaning that only one level of downstream failed builds will be parsed.
+	GetMaxRecursionDepth() int
 }
 
 func ParseFromSource(source LogSource, rules []*Rule, maxMatches int) ([]*ParseMatch, Stats, error) {
