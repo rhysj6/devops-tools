faadf87fbc070b6ec32f8ac10597c781763550d0
rhysj6 <rhysj6@gmail.com>
2026-03-10 23:11:14 +0000

    Added category output

--- pkg/logparser/outputs.go
+++ pkg/logparser/outputs.go
@@ -20,6 +20,9 @@ func TextOutput(w io.Writer, matches []*ParseMatch, stats Stats) {
 
 	for _, m := range matches {
 		_, _ = fmt.Fprintf(w, "Matched rule: %v \n", m.Rule.Name)
+		if m.Rule.Category != "" {
+			_, _ = fmt.Fprintf(w, "Category: %v \n", m.Rule.Category)
+		}
 		_, _ = fmt.Fprintf(w, "Solution: \n%v\n\n", m.Rule.Solution)
 		_, _ = fmt.Fprintf(w, "Log extract: \n\n")
 		for _, l := range m.MatchedLines {
